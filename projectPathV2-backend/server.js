43472fda522027369b7c075c5582c65e26aec409
jeraldjohn <34527907+jeraldjohngalicha@users.noreply.github.com>
2018-11-20 14:34:43 +0800

    Updated server.js
    Updated server.js. Added proper constants inorder for server to run and connect to MongoDB.
    
    MongoDB must be installed for server to run properly and is running on default mongoDB port.

--- projectPathV2-backend/server.js
+++ projectPathV2-backend/server.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import mongoose from 'mongoose';
 
+import profileRoutes from './routes/profileRoutes';
+
 const app = express();
+app.use('/profileRoutes', profileRoutes);
 
-const dbCon = mongoose.connect('mongodb://demoaccount:demoaccount1@ds111244.mlab.com:11244/ppv2');
-
+const dbConn = mongoose.connect('mongodb://localhost:27017');
 
 app.listen(4000, () => {
     console.log('Connected to port 4000');
