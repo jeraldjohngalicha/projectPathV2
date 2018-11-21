cbb4b251a93e07f54245373ba38c63f32e81b7fa
jeraldjohn <34527907+jeraldjohngalicha@users.noreply.github.com>
2018-11-21 16:19:20 +0800

    Update search-page.service.ts

--- projectPathV2-frontend/src/app/service/search-page/search-page.service.ts
+++ projectPathV2-frontend/src/app/service/search-page/search-page.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { map } from "rxjs/operators";
 
 import { RouterUtils } from '../../components/shared/router-utils.component';
 
