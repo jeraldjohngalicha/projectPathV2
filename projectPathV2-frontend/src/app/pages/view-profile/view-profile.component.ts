717c0f89551be253cda818217f3da02df0333e9b
Jessie B <jessie.badilla2448@gmail.com>
2018-11-22 15:49:58 +0800

    Creation of "View Profile"
    Creation of html and ts files

---
+++ projectPathV2-frontend/src/app/pages/view-profile/view-profile.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ngx-view-profile',
+  templateUrl: './view-profile.component.html',
+})
+
+export class ViewProfileComponent {
+}
